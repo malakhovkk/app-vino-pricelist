a5819ba4d2d611aec2187131fa0ad424b8caf79d
Maksim <maksme99@gmail.com>
2025-12-04 16:09:48 +0300

    fix

--- src/pages/deals/deals.js
+++ src/pages/deals/deals.js
@@ -842,7 +842,7 @@ export default function Deals() {
                     // }
                     mydate2 = formatDate(date2, "yyyy-MM-dd");
                   }
-                  alert(status,  deal, statusVP)
+                  // alert(status,  deal, statusVP)
                   const res = await getDeals({
                     shop: shopid,
                     status,
